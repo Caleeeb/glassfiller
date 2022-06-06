58a5500655ce76725864964a31777595e9d1d136
Caleeeb <daycinematography@gmail.com>
2022-06-05 20:40:29 -0400

    post and user routes connected and working, some handelbars started

--- public/javascript/add-recipe.js
+++ public/javascript/add-recipe.js
@@ -1,1 +1,41 @@
-// 
+// async function recipeFormHandler(event) {
+// 	event.preventDefault();
+
+// 	const ingredients = ingredientsArray.map(ingredient => {
+//     name: ingredient.name;
+//     quantity: ingredient.quantity;
+//     unit: ingredient.unit;
+//   })
+
+//   if (comment_text) {
+// 		const response = await fetch("/api/comments", {
+// 			method: "POST",
+// 			body: JSON.stringify({
+//         title,
+// 				ingredients,
+// 				// desciption,
+//         user_id,
+// 			}),
+// 			headers: {
+// 				"Content-Type": "application/json",
+// 			},
+// 		});
+
+// 		if (response.ok) {
+// 			document.location.reload();
+// 		} else {
+// 			alert(response.statusText);
+// 		}
+// 	}
+// }
+
+// async function handleNewIngredient(event) {
+//   const parentContainer = document.querySelector(".ingredient-container");
+//   parentContainer.appendChild('<li class = "ingredient">)
+//   //to do: create the input line for the added ingredient
+
+// }
+
+// const ingredientsArray = document.querySelectorAll(".ingredient")
+// console.log(ingredientsArray);
+// document.onclick(".add-ingredient", handleNewIngredient);
