0c4474509bf7786a493b7e183d569bbe8dcd1fe2
Cardenas1850 <99810565+Cardenas1850@users.noreply.github.com>
2022-06-09 18:11:10 -0400

    design changes

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(require("./controllers"));
-
+/*change back to false? */
 sequelize.sync({ force: true }).then(() => {
 	app.listen(PORT, () => console.log("Now listening"));
 });
